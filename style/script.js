f0967926b32f0cc1ce302888ed53b16beaf5f6ba
Jeremie <bj.06202000@gmail.com>
2023-11-17 10:19:59 +0100

    Starting projects section

--- style/script.js
+++ style/script.js
@@ -1,3 +1,12 @@
+function loop(){
+  var typed = new Typed("#element", {
+      strings: ["m'appelle<br>Jérémie Bayon.", "suis étudiant<br>en informatique.", "suis développeur."],
+      typeSpeed: 75,
+      backSpeed: 20,
+      loop: true,
+      });
+}
+
 function showMobileNavigation(){
   const hamburger = document.querySelector('.hamburger').classList.toggle('open');
   const mobileNav = document.querySelector('.mobile-navigation').classList.toggle('active');
@@ -14,15 +23,6 @@ navLinks.forEach(link => {
     link.addEventListener('click', closeMobileNavigation);
 });
 
-function loop(){
-  var typed = new Typed("#element", {
-      strings: ["m'appelle<br>Jérémie Bayon.", "suis étudiant<br>en informatique.", "suis développeur."],
-      typeSpeed: 75,
-      backSpeed: 20,
-      loop: true,
-      });
-}
-
 const observer = new IntersectionObserver((entries) => {
   entries.forEach((entry) => {
     console.log(entry);
@@ -35,8 +35,11 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el));
+const hiddenElementsX = document.querySelectorAll('.hidden-x');
+hiddenElementsX.forEach((el) => observer.observe(el));
+
+const hiddenElementsY = document.querySelectorAll('.hidden-y');
+hiddenElementsY.forEach((el) => observer.observe(el));
 
 document.addEventListener("DOMContentLoaded", function () {
   const navbarLinks = document.querySelectorAll(".navbar ul li a");
@@ -64,3 +67,59 @@ const observer = new IntersectionObserver((entries) => {
     observer.observe(section);
   });
 });
+
+const buttons = document.querySelectorAll("[data-carousel-button]")
+
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    const offset = button.dataset.carouselButton === "next" ? 1 : -1
+    const slides = button
+      .closest("[data-carousel]")
+      .querySelector("[data-slides]")
+
+    const activeSlide = slides.querySelector("[data-active]")
+    let newIndex = [...slides.children].indexOf(activeSlide) + offset
+    if (newIndex < 0) newIndex = slides.children.length - 1
+    if (newIndex >= slides.children.length) newIndex = 0
+
+    slides.children[newIndex].dataset.active = true
+    delete activeSlide.dataset.active
+  })
+})
+
+const openModalButtons = document.querySelectorAll('[data-modal-target]')
+const closeModalButtons = document.querySelectorAll('[data-close-button]')
+const overlay = document.getElementById('overlay')
+
+openModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget)
+    openModal(modal)
+  })
+})
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active')
+  modals.forEach(modal => {
+    closeModal(modal)
+  })
+})
+
+closeModalButtons.forEach(button => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal')
+    closeModal(modal)
+  })
+})
+
+function openModal(modal) {
+  if (modal == null) return
+  modal.classList.add('active')
+  overlay.classList.add('active')
+}
+
+function closeModal(modal) {
+  if (modal == null) return
+  modal.classList.remove('active')
+  overlay.classList.remove('active')
+}
