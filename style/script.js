d01e38bd866871159c05faf16ef32ac95169ce13
Jeremie <bj.06202000@gmail.com>
2023-11-24 08:34:28 +0100

    Continue building projects section

--- style/script.js
+++ style/script.js
@@ -68,62 +68,6 @@ const observer = new IntersectionObserver((entries) => {
   });
 });
 
-// const buttons = document.querySelectorAll("[data-carousel-button]")
-
-// buttons.forEach(button => {
-//   button.addEventListener("click", () => {
-//     const offset = button.dataset.carouselButton === "next" ? 1 : -1
-//     const slides = button
-//       .closest("[data-carousel]")
-//       .querySelector("[data-slides]")
-
-//     const activeSlide = slides.querySelector("[data-active]")
-//     let newIndex = [...slides.children].indexOf(activeSlide) + offset
-//     if (newIndex < 0) newIndex = slides.children.length - 1
-//     if (newIndex >= slides.children.length) newIndex = 0
-
-//     slides.children[newIndex].dataset.active = true
-//     delete activeSlide.dataset.active
-//   })
-// })
-
-// const openModalButtons = document.querySelectorAll('[data-modal-target]')
-// const closeModalButtons = document.querySelectorAll('[data-close-button]')
-// const overlay = document.getElementById('overlay')
-
-// openModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = document.querySelector(button.dataset.modalTarget)
-//     openModal(modal)
-//   })
-// })
-
-// overlay.addEventListener('click', () => {
-//   const modals = document.querySelectorAll('.modal.active')
-//   modals.forEach(modal => {
-//     closeModal(modal)
-//   })
-// })
-
-// closeModalButtons.forEach(button => {
-//   button.addEventListener('click', () => {
-//     const modal = button.closest('.modal')
-//     closeModal(modal)
-//   })
-// })
-
-// function openModal(modal) {
-//   if (modal == null) return
-//   modal.classList.add('active')
-//   overlay.classList.add('active')
-// }
-
-// function closeModal(modal) {
-//   if (modal == null) return
-//   modal.classList.remove('active')
-//   overlay.classList.remove('active')
-// }
-
 const initSlider = () => {
   const imageList = document.querySelector(".slider-wrapper .image-list");
   const slideButtons = document.querySelectorAll(".slider-wrapper .slide-button");
@@ -164,7 +108,7 @@ const initSlider = () => {
   // Slide images according to the slide button clicks
   slideButtons.forEach(button => {
       button.addEventListener("click", () => {
-          const direction = button.id === "previous-slide" ? -1 : 1;
+          const direction = button.id === "prev-slide" ? -2 : 0.65;
           const scrollAmount = imageList.clientWidth * direction;
           imageList.scrollBy({ left: scrollAmount, behavior: "smooth" });
       });
