b708cc32de544916a34046d0f5f1ec44c16b4d6b
Jérémie Bayon <jeremie.bayon@kozea.fr>
2024-02-09 18:34:20 +0100

    Continue improving style for projects section

--- assets/script.js
+++ assets/script.js
@@ -97,4 +97,15 @@ function setActiveLink(link) {
     }, 5000);
 }
 
+var counter = 2;
+
+setInterval(function(){
+  document.getElementById('btn' + counter).click();
+  counter++;
+  // console.log(counter);
+  if (counter == 4){
+    counter = 1;
+  }
+}, 5000);
+
 changeNavBarStyleOnScroll()
